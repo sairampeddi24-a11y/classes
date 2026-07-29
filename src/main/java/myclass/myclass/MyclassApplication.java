71cb90e745e3881a765bc7c654bc5d6bcf6f1682
sairempeddi24-a11y <sairampeddi24@gmail.com>
2026-07-29 21:55:17 +0530

    kjmmv

--- src/main/java/myclass/myclass/MyclassApplication.java
+++ src/main/java/myclass/myclass/MyclassApplication.java
@@ -1,13 +1,18 @@
 package myclass.myclass;
 
+
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MyclassApplication {
+	
+	public static org.slf4j.Logger log = LoggerFactory.getLogger(MyclassApplication.class);
 
 	public static void main(String[] args) {
 		System.out.println("hello world");
+		log.info("building jenkins");
 		SpringApplication.run(MyclassApplication.class, args);
 	
 	}
