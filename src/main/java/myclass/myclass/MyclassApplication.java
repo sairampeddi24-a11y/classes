6e181a0e09fd32d9c9b454ced8e88ae78efea466
sairempeddi24-a11y <sairampeddi24@gmail.com>
2026-07-29 21:37:55 +0530

    jnvfn

--- src/main/java/myclass/myclass/MyclassApplication.java
+++ src/main/java/myclass/myclass/MyclassApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MyclassApplication {
 
 	public static void main(String[] args) {
+		System.out.println("hello world");
 		SpringApplication.run(MyclassApplication.class, args);
+	
 	}
 
 }
